a93c0a272f494d85d116364c07517838f7c25920
kekuso <kekuso808@gmail.com>
2016-07-20 15:22:38 -1000

    Documented whiteboard solution for normalizing database.

--- normalized.sql
+++ normalized.sql
@@ -1,2 +1,7 @@
 /* Normalized Schema */
 
+/* Whiteboard:
+ *
+ * 1. Create separate tables for each group of make_code.
+ *    Example: All rows with make_code 'ACURA' will be in one distinct table,
+ *    All rows with make_code 'YUGO' will be a separate table, etc.
